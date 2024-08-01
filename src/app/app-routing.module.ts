80db58fb7862453b99cc98ac04838fe42bde5178
roberto-juniorjp <juniorjp@gmail.com>
2024-08-01 18:33:46 -0300

    feat: initial setups and first layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WeatherHomeComponent } from './modules/weather/page/weather-home/weather-home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'weather',
+    pathMatch: 'full',
+  },
+  {
+    path: 'weather',
+    component: WeatherHomeComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/modules/weather/page/weather-home/weather-home.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-home',
+  templateUrl: './weather-home.component.html',
+  styleUrls: [],
+})
+export class WeatherHomeComponent {}
