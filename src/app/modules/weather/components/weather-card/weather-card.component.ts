4277697a08f9c827dc6307f2ee9745edbbccd016
roberto-juniorjp <juniorjp@gmail.com>
2024-08-01 20:24:11 -0300

    chore: clean weather card component files

--- src/app/modules/weather/components/weather-card/weather-card.component.ts
+++ src/app/modules/weather/components/weather-card/weather-card.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-weather-card',
   templateUrl: './weather-card.component.html',
-  styleUrls: ['./weather-card.component.scss']
+  styleUrls: [],
 })
-export class WeatherCardComponent {
-
-}
+export class WeatherCardComponent {}
